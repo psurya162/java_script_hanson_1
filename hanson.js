63adbb01752448e7502465106b2ad561b1a13350
psurya162 <116509382+psurya162@users.noreply.github.com>
2023-08-18 20:07:07 +0530

    time_update

--- hanson.js
+++ hanson.js
@@ -14,3 +14,26 @@ btn2.addEventListener("click",function(){
 btn3.addEventListener("click", function(){
     container.style.flexDirection="column"
 })
+
+function clock(){
+    const date = new Date()
+    let hours = date.getHours()
+    let minutes =date.getMinutes()
+    let seconds =date.getSeconds()
+
+    if(hours>=12){
+        hours=hours-12
+    }
+
+console.log(hours,minutes,seconds)
+
+let hrs =document.getElementById("hourss")
+hrs.innerText=hours
+let min=document.getElementById("minutess")
+min.innerText=minutes
+let sec=document.getElementById("secondss")
+sec.innerText = seconds
+}
+setInterval(()=>{
+    clock()
+},1000)
